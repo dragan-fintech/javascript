9b5b23dd0d30ed1893bd52056898b0f1b173a5f1
Dragan Nikolic <dragan.d.nikolic@gmail.com>
2017-09-05 21:09:09 -0700

    hoisting example

---
+++ hoisting_ex.js
@@ -0,0 +1,36 @@
+/* 
+If we execute this Javascript, what will the browser's console show?
+
+Gotchas:
+It's not "outside".
+
+It's not "inside".
+
+The script won't throw an error!
+
+Hoisting: 
+In Javascript, variable declarations are "hoisted" to the top of the current 
+scope. Variable assignments, however, are not.
+
+The declaration (but not the assignment) of text gets hoisted to the top of 
+logIt(). So our code gets interpreted as though it were:
+
+var text = 'outside';
+function logIt(){
+    var text;
+    console.log(text);
+    text = 'inside';
+};
+logIt();
+
+Answer: 
+Console will show 'undefined'
+*/
+var text = 'outside';
+
+function logIt() {
+    console.log(text);
+    var text = 'inside';
+};
+
+logIt();
