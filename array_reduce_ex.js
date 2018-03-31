eaab32bd918087294a3a475eefcdf9564c544b11
Dragan Nikolic <dragan.d.nikolic@gmail.com>
2018-03-31 12:42:30 +0200

    update array map and reduce

--- array_reduce_ex.js
+++ array_reduce_ex.js
@@ -1,6 +1,11 @@
 /*
 reduce_ex
 
+The reduce() method reduces the array to a single value.
+The reduce() method executes a provided function for each value of the array (from left-to-right).
+The return value of the function is stored in an accumulator (result/total).
+Note: reduce() does not execute the function for array elements without values.
+
 Run: node <file.js>
 
 2017-06-30
@@ -17,6 +22,7 @@ function count_values() {
         }
 
         console.log(`------${name}: ${index}-------------`);
+        console.log(namesArr);
         console.log(countedNamesAcc);
         console.log('------------------------------------');
 
