92209b386c58897cdbeaff05695a6202e7891ffa
Dragan Nikolic <dragan.d.nikolic@gmail.com>
2018-04-17 14:21:37 +0200

    update some examples

--- set_ex.js
+++ set_ex.js
@@ -20,10 +20,16 @@ var arr1 = [1,2,3,1]
 print_collection(arr1, "arr1")
 var arr2 = [3,2,1]
 print_collection(arr2, "arr2")
+var arr3 = [1,3,2]
+print_collection(arr3, "arr3")
 var set1 = new Set(arr1)
 print_collection(set1, "set1")
 var set2 = new Set(arr2)
 print_collection(set2, "set2")
+var set3 = new Set(arr3)
+print_collection(set3, "set3")
 
 console.log(`arr1 == arr2? ${_.isEqual(arr1, arr2)}`)  // false
 console.log(`set1 == set2? ${_.isEqual(set1, set2)}`)  // true
+console.log(`arr2 == arr3? ${_.isEqual(arr2, arr3)}`)  // false
+console.log(`set3 == set2? ${_.isEqual(set3, set2)}`)  // true
--- yaml_ex/ex_load_singe_file.js
+++ yaml_ex/ex_load_singe_file.js
@@ -5,8 +5,8 @@ fs   = require('fs');
 
 // Get document, or throw exception on error
 try {
-  var doc = yaml.safeLoad(fs.readFileSync('td_linux.yml', 'utf8'));
-  console.log(doc);
+  var doc = yaml.safeLoad(fs.readFileSync('td_windows.yml', 'utf8'));
+  console.log(JSON.stringify(doc));
 } catch (e) {
   console.log(e);
 }
