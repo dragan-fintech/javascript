61aeddacc4fbb21a604e817438e9b4feddb4cfa2
Dragan Nikolic <dragan.d.nikolic@gmail.com>
2018-08-28 13:38:11 +0200

    add standard check

--- yaml_ex/ex_load_mulitple_files.js
+++ yaml_ex/ex_load_mulitple_files.js
@@ -1,16 +1,16 @@
 // load multiple yml files example
 
-const yaml = require('js-yaml');
-const fs = require('fs');
-const _ = require('lodash');
+const yaml = require('js-yaml')
+const fs = require('fs')
+const _ = require('lodash')
 
 // load and merge multipe yaml files
 try {
-  const data1 = yaml.safeLoad(fs.readFileSync('td_linux.yml', 'utf8'));
-  const data2 = yaml.safeLoad(fs.readFileSync('td_accounts.yml', 'utf8'));
+  const data1 = yaml.safeLoad(fs.readFileSync('td_linux.yml', 'utf8'))
+  const data2 = yaml.safeLoad(fs.readFileSync('td_accounts.yml', 'utf8'))
   const data = _.merge(data1, data2)
 
-  console.log(data);
+  console.log(data)
 } catch (e) {
-  console.log(e);
+  console.log(e)
 }
--- yaml_ex/ex_load_singe_file.js
+++ yaml_ex/ex_load_singe_file.js
@@ -1,12 +1,12 @@
 // load a single yml file example
 
-yaml = require('js-yaml');
-fs   = require('fs');
+const yaml = require('js-yaml')
+const fs = require('fs')
 
 // Get document, or throw exception on error
 try {
-  var doc = yaml.safeLoad(fs.readFileSync('td_windows.yml', 'utf8'));
-  console.log(JSON.stringify(doc));
+  var doc = yaml.safeLoad(fs.readFileSync('td_windows.yml', 'utf8'))
+  console.log(JSON.stringify(doc))
 } catch (e) {
-  console.log(e);
+  console.log(e)
 }
